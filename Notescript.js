38c6b2789e80ab056006c56d6d649b819b6a66af
saute-fest <56605759+saute-fest@users.noreply.github.com>
2020-01-10 14:32:02 -0500

    Jeffery you make my brain hurt

--- Notescript.js
+++
@@ -1,7 +0,0 @@
-function mB() {
-  var height = document.getElementsByClassName("bottom").style.height;
-  var newX = screen.height - height;
-  var cssString = newX.toString();
-  cssString = "translateY(" + cssString + ")";
-  document.getElementsByClassName("bottom").style.transform = cssString;
-}
