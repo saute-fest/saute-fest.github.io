2439464aa5a2f2cf141c3ed1522cf9d4c97d35d5
Jeffrey <jeffreyhuang08@gmail.com>
2020-01-15 11:02:49 -0500

    uploaded past csss

---
+++ init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBc_XQ7jVVQxkt2lCBpuk4E13ksvR5OonA",
+  "appId": "1:741800372698:web:816c50ed2c237f4eb8b8e3",
+  "authDomain": "commentarium-c0882.firebaseapp.com",
+  "databaseURL": "https://commentarium-c0882.firebaseio.com",
+  "measurementId": "G-RJXZB44RZH",
+  "messagingSenderId": "741800372698",
+  "projectId": "commentarium-c0882",
+  "storageBucket": "commentarium-c0882.appspot.com"
+});
