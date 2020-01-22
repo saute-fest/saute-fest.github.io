049ca80c3a6e39ed9e479823a2a54ae89c4daf55
Vedo9973 <jackandandrew20041@gmail.com>
2020-01-22 16:18:26 -0500

    Update steve.py

--- steve.py
+++ steve.py
@@ -6,4 +6,17 @@ crsr = connection.cursor()
 
 crsr.execute("remotemysql.com")
 
-crsr.execute("SELECT * FROM remotemysql.com")  
+import sqlite3
+
+connection = sqlite3.connect("remotemysql.com") 
+#connecting to the SQL database ^
+crsr = connection.cursor() 
+
+crsr.execute("SELECT * FROM emp")  
+
+# store all the fetched data in the ans variable 
+ans= crsr.fetchall()  
+  
+# loop to print all the data 
+for i in ans: 
+    print(i)   
