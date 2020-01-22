82ac83e2f7a1faa728a009e45bb99a16b2956e4b
saute-fest <56605759+saute-fest@users.noreply.github.com>
2020-01-22 14:09:27 -0500

    IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- fos/upload.php
+++ fos/upload.php
@@ -1,24 +1,28 @@
 <?php 
-
-$file = $_FILES['file'];
-$name = $file['name'];
-$path = "../documents" . basename($name);
-if (move_uploaded_file($file['tmp_name'], $path)) {
-    // Move succeed.
-} else {
-    // Move failed. Possible duplicate?
-}
-$servername = "remotemysql.com";
-$username = "47l2llZFr2";
-$password = "92Mb7L9qCW";
-$dbname = "47l2llZFr2";
+//DONT U DARE FUCKING UPLOAD THE VARS TO IT HUB BTW THIS IS WHERE U PUT THEM
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+$target_dir = "../documents/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
     
 
-    $sql = 'INSERT INTO file VALUES ("test", "$path", "test");';
+    $sql = "INSERT INTO file 
+    VALUES (DEFAULT, 'hfif', '$target_file', 'hfi')";
     if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
