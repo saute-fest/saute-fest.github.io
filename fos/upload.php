9d2711aa97c1558ca8e76a646ffe99d0e1bfdbc5
saute-fest <56605759+saute-fest@users.noreply.github.com>
2020-01-22 21:30:43 -0500

    FANCY UPLOADING XMLHttp AAAAAAAAAAAAAAAAH

--- fos/upload.php
+++ fos/upload.php
@@ -1,32 +1,47 @@
-<?php 
-//DONT U DARE FUCKING UPLOAD THE VARS TO IT HUB BTW THIS IS WHERE U PUT THEM
+<?php
+//VARS PINNED IN THE DISCORD
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+    
 
+   
 $target_dir = "../documents/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
 
-    
-
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "pdf" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
     $sql = "INSERT INTO file 
     VALUES (DEFAULT, 'hfif', '$target_file', 'hfi')";
     if (mysqli_query($conn, $sql)) {
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
-        }
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo "Sorry, there was an error uploading your file.";
     }
-    
-    mysqli_close($conn);
+}
+}
 
-?>
+        ?>
+    
