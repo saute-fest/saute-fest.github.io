a68441d79059eb7bd82b6a9af358593a3561ae3f
Vedo9973 <jackandandrew20041@gmail.com>
2020-01-23 19:50:12 -0500

    Update steve.py

--- steve.py
+++ steve.py
@@ -1,14 +1,15 @@
 import sqlite3
 
 connection = sqlite3.connect("remotemysql.com") 
-#connecting to the SQL database ^
+# connecting to the SQL database ^
 crsr = connection.cursor() 
 
 crsr.execute("SELECT * FROM ")  
+# I need access to SQL for Steve to do his job^
 
-# store all the fetched data in the ans variable 
 ans= crsr.fetchall()  
-  
-# loop to print all the data 
+# store all the fetched data in the ans variable^
+
 for i in ans: 
-    print(i)   
+    print(i) 
+# loop to print all the data ^  
