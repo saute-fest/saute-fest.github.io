8d3969d9bf17ad585b7b6f759156e7de922be63b
Jeffrey <jeffreyhuang08@gmail.com>
2020-01-29 14:22:21 -0500

    oooooooooof

---
+++ experiments/load.js
@@ -0,0 +1,15 @@
+setTimeout(function () {
+        var r =
+        `<h1>History</h1>
+        <h2>Search Classes</h2>
+        <form name="search">
+          <input type="text"></input>
+        </form>
+        <h2>Upcoming Tests</h2>
+        <div></div>
+        <a href="../index.html">Back</a>
+      </div>
+    `;
+
+          document.getElementById("msg").innerHTML = r;
+    }, 400);
